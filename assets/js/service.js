c44bb4358407dbf9be86ef355c15d79da112f58c
mahmoudAcm <mahmoud.acm.123@gamil.com>
2020-12-10 12:29:16 +0200

    adding responsiveness to service section

---
+++ assets/js/service.js
@@ -0,0 +1,33 @@
+
+const serviceCard = ({ title, bg, rest }) => `
+<div class="card ${rest? 'shadow': 'shadow-sm'}">
+    <div class="card-body">
+        <div>
+            <div class="service-circle ${bg}"></div>
+        </div>
+        <div class="main-content-right">
+            <h5>${title}</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</div>
+`;
+
+const serviceItems = [
+{
+    title: 'Excellent protection',
+    bg: 'bg-third',
+    rest: true
+},
+{
+    title: 'High savings potential',
+    bg: 'bg-second'
+},
+{
+    title: 'Maximum time savings',
+    bg: 'bg-first'
+}
+];
+
+const serviceCardContainer = $(".service-first .main-content");
+serviceCardContainer.append(serviceItems.map(item => serviceCard(item)).join(""));
